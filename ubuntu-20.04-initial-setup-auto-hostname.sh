b86665d0c8553394e47daece45a5cfb982688972
Sheraz Ahmed <sheraz.ahmed@vaival.com>
2022-12-29 03:08:41 +0500

    Update ubuntu-20.04-initial-setup-auto-hostname.sh

--- ubuntu-20.04-initial-setup-auto-hostname.sh
+++ ubuntu-20.04-initial-setup-auto-hostname.sh
@@ -14,10 +14,8 @@ cat /home/ubuntu/.ssh/authorized_keys | tee ~/.ssh/authorized_keys
 
 cat /home/ubuntu/.bashrc | tee ~/.bashrc
 
-cat <<EOF | tee -a ~/.bashrc
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]$PWD\[\033[00m\]\n\$ '
-PS1=$PS1"\[\e]0;`hostname`\a\]"
-EOF
+echo "PS1='\n`uptime`\n\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\$PWD\[\033[00m\]\n\$ '" >> ~/.bashrc
+echo 'PS1=$PS1"\[\e]0;`hostname`\a\]"' >> ~/.bashrc
 
 swapoff -a; sed -i '/swap/d' /etc/fstab
 
