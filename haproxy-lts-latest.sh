cc3a93657e4793410b78e6bdbd9370e779b50f62
Sheraz Ahmed <sheraz.ahmed@vaival.com>
2023-01-16 09:10:31 +0500

    Update haproxy-lts-latest.sh

--- haproxy-lts-latest.sh
+++ haproxy-lts-latest.sh
@@ -1,3 +1,7 @@
+###########################################
+# https://en.wikipedia.org/wiki/HAProxy
+# 2.6 LTS	2022-05-31	2027-Q2 (LTS)
+###########################################
 sudo add-apt-repository ppa:vbernat/haproxy-2.6 -y
 sudo apt update
 
